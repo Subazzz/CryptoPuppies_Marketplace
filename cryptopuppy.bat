1349a61e8a4069e0484a48f79d4f9d3a188122f3
Subazzz <92784361+Subazzz@users.noreply.github.com>
2022-01-07 15:45:04 +0530

    Updated

--- cryptopuppy.bat
+++ cryptopuppy.bat
@@ -1,5 +1,2 @@
-git clone https://github.com/vmieres/CryptoPuppies_Marketplace.git
-cd CryptoPuppies_Marketplace
-npm install
-npm i -g truffle
-npm i web3
+npm install && npm i -g truffle && npm i web3
+exit
